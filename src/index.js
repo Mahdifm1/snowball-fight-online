ebdc76aa3ef64d811b661745b8c58774092adb71
Mahdi Lotfian <80mahde@gmail.com>
2024-01-27 00:18:14 +0330

    fix chat bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const {createServer} = require("http");
-const {Server} = require("socket.io");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
@@ -23,167 +23,178 @@ const inputsMap = {};
 let ground2D, decal2D;
 
 function isColliding(rect1, rect2) {
-    return (
-        rect1.x < rect2.x + rect2.w &&
-        rect1.x + rect1.w > rect2.x &&
-        rect1.y < rect2.y + rect2.h &&
-        rect1.h + rect1.y > rect2.y
-    );
+  return (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.h + rect1.y > rect2.y
+  );
 }
 
 function isCollidingWithMap(player) {
-    for (let row = 0; row < decal2D.length; row++) {
-        for (let col = 0; col < decal2D[0].length; col++) {
-            const tile = decal2D[row][col];
+  for (let row = 0; row < decal2D.length; row++) {
+    for (let col = 0; col < decal2D[0].length; col++) {
+      const tile = decal2D[row][col];
 
-            if (
-                tile &&
-                isColliding(
-                    {
-                        x: player.x,
-                        y: player.y,
-                        w: 32,
-                        h: 32,
-                    },
-                    {
-                        x: col * TILE_SIZE,
-                        y: row * TILE_SIZE,
-                        w: TILE_SIZE,
-                        h: TILE_SIZE,
-                    }
-                )
-            ) {
-                return true;
-            }
-        }
+      if (
+        tile &&
+        isColliding(
+          {
+            x: player.x,
+            y: player.y,
+            w: 32,
+            h: 32,
+          },
+          {
+            x: col * TILE_SIZE,
+            y: row * TILE_SIZE,
+            w: TILE_SIZE,
+            h: TILE_SIZE,
+          }
+        )
+      ) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function tick(delta) {
+  for (const player of players) {
+    const inputs = inputsMap[player.id];
+    const previousY = player.y;
+    const previousX = player.x;
+
+    if (inputs.up) {
+      player.y -= SPEED;
+    } else if (inputs.down) {
+      player.y += SPEED;
+    }
+
+    if (isCollidingWithMap(player)) {
+      player.y = previousY;
+    }
+
+    if (inputs.left) {
+      player.x -= SPEED;
+    } else if (inputs.right) {
+      player.x += SPEED;
+    }
+
+    if (isCollidingWithMap(player)) {
+      player.x = previousX;
+    }
+  }
+
+  for (const snowball of snowballs) {
+    snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
+    snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
+    snowball.timeLeft -= delta;
+
     for (const player of players) {
-        const inputs = inputsMap[player.id];
-        const previousY = player.y;
-        const previousX = player.x;
-
-        if (inputs.up) {
-            player.y -= SPEED;
-        } else if (inputs.down) {
-            player.y += SPEED;
-        }
-
-        if (isCollidingWithMap(player)) {
-            player.y = previousY;
-        }
-
-        if (inputs.left) {
-            player.x -= SPEED;
-        } else if (inputs.right) {
-            player.x += SPEED;
-        }
-
-        if (isCollidingWithMap(player)) {
-            player.x = previousX;
-        }
+      if (player.id === snowball.playerId) continue;
+      const distance = Math.sqrt(
+        (player.x + PLAYER_SIZE / 2 - snowball.x) ** 2 +
+          (player.y + PLAYER_SIZE / 2 - snowball.y) ** 2
+      );
+      if (distance <= PLAYER_SIZE / 2) {
+        player.x = 0;
+        player.y = 0;
+        snowball.timeLeft = -1;
+        break;
+      }
     }
+  }
+  snowballs = snowballs.filter((snowball) => snowball.timeLeft > 0);
 
-    for (const snowball of snowballs) {
-        snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
-        snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
-        snowball.timeLeft -= delta;
-
-        for (const player of players) {
-            if (player.id === snowball.playerId) continue;
-            const distance = Math.sqrt(
-                (player.x + PLAYER_SIZE / 2 - snowball.x) ** 2 +
-                (player.y + PLAYER_SIZE / 2 - snowball.y) ** 2
-            );
-            if (distance <= PLAYER_SIZE / 2) {
-                player.x = 0;
-                player.y = 0;
-                snowball.timeLeft = -1;
-                break;
-            }
-        }
-    }
-    snowballs = snowballs.filter((snowball) => snowball.timeLeft > 0);
-
-    io.emit("players", players);
-    io.emit("snowballs", snowballs);
+  io.emit("players", players);
+  io.emit("snowballs", snowballs);
 }
 
+const chatChannel = io.of('/chat');
+
+chatChannel.on('connect', (socket) => {
+    console.log('User connected to chat channel', socket.id);
+
+    socket.on('chat message', (message) => {
+        console.log('Chat message received:', message);
+
+        // Broadcast the message to all connected users
+        chatChannel.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected from chat channel');
+    });
+});
+
+
 async function main() {
-    ({ground2D, decal2D} = await loadMap());
+  ({ ground2D, decal2D } = await loadMap());
 
-    io.on("connect", (socket) => {
-        console.log("user connected", socket.id);
+  io.on("connect", (socket) => {
+    console.log("user connected", socket.id);
 
-        socket.on('chat message', (message) => {
-            console.log('Chat message received:', message);
+    inputsMap[socket.id] = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
 
-            // Notify all connected clients about the new chat message
-            io.emit('chat message', message);
+    players.push({
+      id: socket.id,
+      x: 800,
+      y: 800,
+    });
 
+    socket.emit("map", {
+      ground: ground2D,
+      decal: decal2D,
+    });
 
-            inputsMap[socket.id] = {
-                up: false,
-                down: false,
-                left: false,
-                right: false,
-            };
+    socket.on("inputs", (inputs) => {
+      inputsMap[socket.id] = inputs;
+    });
 
-            players.push({
-                id: socket.id,
-                x: 800,
-                y: 800,
-            });
+    // socket.on("mute", (isMuted) => {
+    //   const player = players.find((player) => player.id === socket.id);
+    //   player.isMuted = isMuted;
+    // });
 
-            socket.emit("map", {
-                ground: ground2D,
-                decal: decal2D,
-            });
+    // socket.on("voiceId", (voiceId) => {
+    //   const player = players.find((player) => player.id === socket.id);
+    //   player.voiceId = voiceId;
+    // });
 
-            socket.on("inputs", (inputs) => {
-                inputsMap[socket.id] = inputs;
-            });
+    socket.on("snowball", (angle) => {
+      const player = players.find((player) => player.id === socket.id);
+      snowballs.push({
+        angle,
+        x: player.x,
+        y: player.y,
+        timeLeft: 1000,
+        playerId: socket.id,
+      });
+    });
 
-            // socket.on("mute", (isMuted) => {
-            //   const player = players.find((player) => player.id === socket.id);
-            //   player.isMuted = isMuted;
-            // });
+    socket.on("disconnect", () => {
+      players = players.filter((player) => player.id !== socket.id);
+    });
+  });
 
-            // socket.on("voiceId", (voiceId) => {
-            //   const player = players.find((player) => player.id === socket.id);
-            //   player.voiceId = voiceId;
-            // });
+  app.use(express.static("public"));
 
-            socket.on("snowball", (angle) => {
-                const player = players.find((player) => player.id === socket.id);
-                snowballs.push({
-                    angle,
-                    x: player.x,
-                    y: player.y,
-                    timeLeft: 1000,
-                    playerId: socket.id,
-                });
-            });
+  httpServer.listen(PORT);
 
-            socket.on("disconnect", () => {
-                players = players.filter((player) => player.id !== socket.id);
-            });
-        });
+  let lastUpdate = Date.now();
+  setInterval(() => {
+    const now = Date.now();
+    const delta = now - lastUpdate;
+    tick(delta);
+    lastUpdate = now;
+  }, 1000 / TICK_RATE);
+}
 
-        app.use(express.static("public"));
-
-        httpServer.listen(PORT);
-
-        let lastUpdate = Date.now();
-        setInterval(() => {
-            const now = Date.now();
-            const delta = now - lastUpdate;
-            tick(delta);
-            lastUpdate = now;
-        }, 1000 / TICK_RATE);
-    }
-
-    main();
+main();
